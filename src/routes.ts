5c0b0a4a85dc741d2c1e660e97272a849bbab9dc
Vianna <vianna8523@gmail.com>
2021-01-07 19:00:20 -0300

    Listar Imagens backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,18 @@
 import express from 'express'
-
+import knex from 'knex'
 const routes = express.Router()
 
-routes.get('/users', (req, resp) => {
-  resp.json({ message: 'Hello World' })
+routes.get('/items', async (req, resp) => {
+  const items = await knex('items').select('*');
+  
+  const serializedItems = items.map(item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+
+  return resp.json(serializedItems)
 })
 
 export default routes
